910908b0b13bcffd7e494e6784af7018eba28504
Pankhuri Kumar <pankhurikumar23@gmail.com>
2018-07-18 20:58:28 -0400

    added credentials file

---
+++ socrata_credentials.py
@@ -0,0 +1,8 @@
+#######
+# Date created: 01/30/2018
+#######
+
+# Replace the value in quotes with your own token and secret
+# The code will not work without this replacement
+app_token = "YOUR_APP_TOKEN"
+secret_token = "YOUR_SECRET_TOKEN"
